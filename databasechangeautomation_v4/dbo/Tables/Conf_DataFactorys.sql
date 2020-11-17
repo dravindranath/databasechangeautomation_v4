5eeee4e18ac745f9274f5a4381b2cf777639829c
dravindranath <02700449@WLCB6TPM33>
2020-11-17 16:30:08 +0530

    Added new column to conf_datafactories

--- databasechangeautomation_v4/dbo/Tables/Conf_DataFactorys.sql
+++ databasechangeautomation_v4/dbo/Tables/Conf_DataFactorys.sql
@@ -3,6 +3,7 @@
     [DataFactoryName]   NVARCHAR (200) NOT NULL,
     [ResourceGroupName] NVARCHAR (200) NOT NULL,
     [Description]       NVARCHAR (MAX) NULL,
+    [Description_GitTest1]       NVARCHAR (MAX) NULL
     CONSTRAINT [PK_DataFactorys] PRIMARY KEY CLUSTERED ([DataFactoryId] ASC)
 );
 
