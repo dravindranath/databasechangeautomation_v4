96676f9bce0818515d6088fc4f96bdf364a641ca
dravindranath <02700449@WLCB6TPM33>
2020-11-17 10:12:40 +0530

    Add project files.

---
+++ databasechangeautomation_v4/dbo/Tables/Conf_Properties.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[Conf_Properties] (
+    [PropertyId]              INT            IDENTITY (1, 1) NOT NULL,
+    [PropertyName]            VARCHAR (128)  NOT NULL,
+    [PropertyValue]           NVARCHAR (MAX) NOT NULL,
+    [Description]             NVARCHAR (MAX) NULL,
+    [ValidFrom]               DATETIME       CONSTRAINT [DF_Properties_ValidFrom] DEFAULT (getdate()) NOT NULL,
+    [ValidTo]                 DATETIME       NULL,
+    [DataDomain]              VARCHAR (128)  NULL,
+    [CheckedInGitStatus]      NCHAR (10)     NULL,
+    [CheckedInGitStatus_Test] NCHAR (10)     NULL,
+    CONSTRAINT [PK_Properties] PRIMARY KEY CLUSTERED ([PropertyId] ASC, [PropertyName] ASC)
+);
+
