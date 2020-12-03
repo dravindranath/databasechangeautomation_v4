34be58ba8a9090da598a9fd69db0cda56f3a24c0
dravindranath <02700449@WLCB6TPM33>
2020-12-03 15:58:34 +0530

    Testing usp_getlastrunstatus_test1

--- databasechangeautomation_v4/dbo/Stored Procedures/usp_getlastrunstatus_test1.sql
+++ databasechangeautomation_v4/dbo/Stored Procedures/usp_getlastrunstatus_test1.sql
@@ -2,8 +2,9 @@
 
 
 
-CREATE PROCEDURE [dbo].[usp_getlastrunstatus_testing321321]
+CREATE PROCEDURE [dbo].[usp_getlastrunstatus_test1]
 AS
 BEGIN
 	Drop table dbo.Dummy_Test2
+	Print 'Adding sample code Test 1'
 END;
